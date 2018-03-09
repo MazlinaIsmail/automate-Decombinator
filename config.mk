4407ac0fdd7699c7fac885098dc956db9503ec14
MazlinaIsmail <mazlina.i@gmail.com>
2018-03-09 16:03:09 +0000

    Add files via upload

--- config.mk
+++ config.mk
@@ -1,13 +1,13 @@
 # Py scripts for Decombinator analysis pipeline
 
-DEMULTIPLEX_SRC = ./Decombinator-master/Demultiplexor.py
+DEMULTIPLEX_SRC = /Volumes/BF_MI_1/Decombinator/Decombinator_M13/Demultiplexor_M13-V1.py
 DEMULTIPLEX_EXE = python $(DEMULTIPLEX_SRC)
 
-DCR_SRC = ./Decombinator-master/Decombinator.py
+DCR_SRC = /Volumes/BF_MI_1/Decombinator/Decombinator_M13/Decombinator_M13-V1.py
 DCR_EXE = python $(DCR_SRC)
 
-COLLAPSE_SRC = ./Decombinator-master/Collapsinator.py
+COLLAPSE_SRC = /Volumes/BF_MI_1/Decombinator/Decombinator_M13/Collapsinator_M13-V1.py
 COLLAPSE_EXE = python $(COLLAPSE_SRC)
 
-TRANSLATE_SRC = ./Decombinator-master/CDR3translator.py
+TRANSLATE_SRC = /Volumes/BF_MI_1/Decombinator/CDR3translator.py
 TRANSLATE_EXE = python $(TRANSLATE_SRC)
